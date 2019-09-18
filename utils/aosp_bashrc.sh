9e700dd25f291d95d7c7e9f3d7a456597f0689b0
erwin.chang <m9207216@mail.ntust.edu.tw>
2019-09-18 11:42:22 +0800

    remove GLSDK env

--- utils/aosp_bashrc.sh
+++ utils/aosp_bashrc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH=${PATH}:/opt/gcc-linaro-arm-linux-gnueabihf-4.7-2013.03-20130313_linux/bin
 export TT_DWORK=${WORK_DIR}
 
 alias dwork='cd ${WORK_DIR}'
@@ -9,11 +8,4 @@ echo "command list"
 echo "dwork: cd workspace dir"
 echo ""
 
-echo ""
-echo "need to setup GLSDK....ex. export GLSDK="
-echo ""
-[ -d ${WORK_DIR}/ti-glsdk_dra7xx-evm_7_04_00_03 ] && {
-    export GLSDK="${WORK_DIR}/ti-glsdk_dra7xx-evm_7_04_00_03"
-    echo "GLSDK: $GLSDK"
-}
 dwork
