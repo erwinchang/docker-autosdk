4ed494515c259d92d25bfc8b094d3cb166f3e9b0
erwin.chang <m9207216@mail.ntust.edu.tw>
2019-09-18 11:43:42 +0800

    patch SSL CA cert fail

--- utils/aosp_bashrc.sh
+++ utils/aosp_bashrc.sh
@@ -8,4 +8,8 @@ echo "command list"
 echo "dwork: cd workspace dir"
 echo ""
 
+#fix wget http://commondatastorage.googleapis.com/git-repo-downloads/repo repo-temp fail
+#unable to access 'https://gerrit.googlesource.com/git-repo/': Problem with the SSL CA cert
+git config --global --add http.sslVerify false
+
 dwork
