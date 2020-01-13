82dd697d85c290276c2db284b79f5564aac111de
erwin.chang <m9207216@mail.ntust.edu.tw>
2020-01-13 17:43:15 +0800

    setting TZ, USER_ID, GROUP_ID environment

--- docker-run.sh
+++ docker-run.sh
@@ -15,4 +15,4 @@ cdir="$vdir/ccache"
 mdir="/home/${USER}/$dirx"
 #home is workdir
 echo "docker run -e WORK_DIR=/home/aosp -v $mdir:/home/aosp -it --rm --name $name erwinchang/autosdk /bin/bash"
-docker run -e WORK_DIR=/home/aosp -v $mdir:/home/aosp -it --rm --name $name erwinchang/autosdk /bin/bash
+docker run -e WORK_DIR=/home/aosp -e TZ=Asia/Taipei -e USER_ID=1000 -e GROUP_ID=1000 -v $mdir:/home/aosp -it --rm --name $name erwinchang/autosdk /bin/bash
